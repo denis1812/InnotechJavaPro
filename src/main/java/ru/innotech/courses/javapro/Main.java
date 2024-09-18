d280b17bf257f44c983e3a76722e545b2629b86c
Denis Davydov <denis812@live.ru>
2024-09-18 20:07:38 +0300

    Initial commit

---
+++ src/main/java/ru/innotech/courses/javapro/Main.java
@@ -0,0 +1,13 @@
+package ru.innotech.courses.javapro;
+
+public class Main {
+    public static void main (String[] args) {
+        System.out.println(
+                "******************************************\n" +
+                "* Courses name: Java Advanced (Module 1) *\n" +
+                "* Company name: Innotech                 *\n" +
+                "* Student:      Denis Davydov            *\n" +
+                "* Start date:   2004/09/10               *\n" +
+                "******************************************");
+    }
+}
